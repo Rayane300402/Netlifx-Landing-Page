a8eebff418043c834df4e5cc616d812f3983e4d0
Rayane Naboulsi <raycaratboulsi@gmail.com>
2024-04-05 16:24:50 +0300

    Update CSS and JavaScript files

--- new.js
+++ new.js
@@ -38,3 +38,45 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const emailInput = document.querySelector("#email-2");
+    const placeholder = document.querySelector("#placeholder-2");
+    const warning = document.querySelector("#warning-2");
+  
+    function updatePlaceholder() {
+      if (emailInput.value) {
+        placeholder.classList.add("has-content");
+      } else {
+        placeholder.classList.remove("has-content");
+      }
+    }
+  
+    function validateEmail() {
+      const email = emailInput.value;
+      if (email.includes("@") && email.includes(".")) {
+        warning.classList.add("hide");
+        emailInput.classList.remove("invalid");
+      } else if (email.length >= 4 && !email.includes("@")) {
+        warning.classList.remove("hide");
+        emailInput.classList.add("invalid");
+        warning.innerHTML =
+          '<i class="fa-solid fa-xmark"></i> Please enter a valid email address';
+      } else if (email.length <= 3 && !email.includes("@")) {
+        warning.classList.remove("hide");
+        emailInput.classList.add("invalid");
+        warning.innerHTML = '<i class="fa-solid fa-xmark"></i> Email required';
+      }
+    }
+  
+    emailInput.addEventListener("input", updatePlaceholder);
+    emailInput.addEventListener("blur", updatePlaceholder);
+  
+    emailInput.addEventListener("input", validateEmail);
+    emailInput.addEventListener("blur", () => {
+      if (emailInput.value === "") {
+        validateEmail();
+      }
+    });
+  });
